8b343d108dd7ed4f7ede5fb4fbce816e782eac54
billz <billzhao1030@gmail.com>
2021-12-26 09:36:22 +1100

    Update the hero image

--- Screen.py
+++ Screen.py
@@ -6,8 +6,10 @@ pygame.init()
 screen = pygame.display.set_mode((480, 700))
 
 background = pygame.image.load("./images/background.png")
+hero = pygame.image.load("./images/me1.png")
 
 screen.blit(background, (0, 0))  # Top-left
+screen.blit(hero, (200, 500))
 
 pygame.display.update()
 
