1de2da0dac94a197abd1fc66befc01b8469483fe
billz <billzhao1030@gmail.com>
2021-12-26 09:25:43 +1100

    Initial commit

---
+++ Player.py
@@ -0,0 +1,3 @@
+import pygame
+
+player_rect = pygame.Rect(100, 500, 120, 125)
---
+++ Screen.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((480, 700))
+while True:
+    pass
+pygame.quit()
+
