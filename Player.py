480e9d09068822fde2df62db6e8cfacbff1c1b2a
billz <billzhao1030@gmail.com>
2021-12-26 09:30:20 +1100

    Update the background image

--- Player.py
+++
@@ -1,3 +0,0 @@
-import pygame
-
-player_rect = pygame.Rect(100, 500, 120, 125)
--- Screen.py
+++ Screen.py
@@ -2,8 +2,16 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((480, 700))
+# Set screen to 480 * 700
+screen = pygame.display.set_mode((480, 700))
+
+background = pygame.image.load("./images/background.png")
+
+screen.blit(background, (0, 0))  # Top-left
+
+pygame.display.update()
+
 while True:
     pass
-pygame.quit()
 
+pygame.quit()
