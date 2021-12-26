71ca49706e1519949bdb623796abc3d951b910c1
billz <billzhao1030@gmail.com>
2021-12-26 12:44:18 +1100

    Motion of hero

--- Screen.py
+++ Screen.py
@@ -11,7 +11,7 @@ background = pygame.image.load("./images/background.png")
 hero = pygame.image.load("./images/me1.png")
 
 screen.blit(background, (0, 0))  # Top-left
-screen.blit(hero, (200, 500))
+screen.blit(hero, (150, 300))
 
 # After draw everything update the screen
 pygame.display.update()
@@ -19,11 +19,22 @@ pygame.display.update()
 # Clock object
 clock = pygame.time.Clock()
 
-
+hero_rect = pygame.Rect(150, 300, 102, 126)
 
 while True:
-
+    # set rate 60
     clock.tick(60)
+
+    hero_rect.y -= 1
+
+    if hero_rect.y <= -hero_rect.height:
+        hero_rect.y = 700
+
+    screen.blit(background, (0, 0))
+    screen.blit(hero, hero_rect)
+
+    pygame.display.update()
+
     pass
 
 pygame.quit()  # Game quit
