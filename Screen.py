4eafa8fd084fe12f7f5de9764618b06510990cca
billz <billzhao1030@gmail.com>
2021-12-26 12:35:25 +1100

    Set clock

--- Screen.py
+++ Screen.py
@@ -1,19 +1,29 @@
 import pygame
 
-pygame.init()
+pygame.init()  # Game initiation
 
 # Set screen to 480 * 700
 screen = pygame.display.set_mode((480, 700))
 
+
+# Load the pictures
 background = pygame.image.load("./images/background.png")
 hero = pygame.image.load("./images/me1.png")
 
 screen.blit(background, (0, 0))  # Top-left
 screen.blit(hero, (200, 500))
 
+# After draw everything update the screen
 pygame.display.update()
 
+# Clock object
+clock = pygame.time.Clock()
+
+
+
 while True:
+
+    clock.tick(60)
     pass
 
-pygame.quit()
+pygame.quit()  # Game quit
